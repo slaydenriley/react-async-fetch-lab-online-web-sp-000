d1cae46c812d0e7271e44c0c95a037de6f442fe2
Riley Slayden <riley@android-3f1f0acbd1dc61cc.localdomain>
2021-01-03 11:01:29 -0700

    commit tree d104886600d54a78ae5b2fca271405c59cd14a78

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
 // create your App component here
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    console.log("test")
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(resp => resp.json())
+    .then(resp => console.log(resp))
+  }
+}
